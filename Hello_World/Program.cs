c03a3154961473b4524002f78922bd4bba4e9e9a
pratikyadav44 <122585419+pratikyadav44@users.noreply.github.com>
2023-05-05 11:21:12 +0530

    updated on remote

--- Hello_World/Program.cs
+++ Hello_World/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("modified on the remote server");
         }
     }
 }
